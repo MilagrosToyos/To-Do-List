69dadb866fbd88ed01eb4a6e802f4e7ed01a4f55
Milagros Toyos <95191805+MilagrosToyos@users.noreply.github.com>
2025-01-06 18:14:46 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -4,62 +4,53 @@ function hideElement(element){
 }
 
 /*funcion  que al  apretar "add" se cree un nuevo item de lista*/
-function addItem(){
-    var li = document.createElement("li");
-    
-    var innput = document.getElementById("btnAdd");
-    li.innerHTML = innput.value;
-    innput.value = "";
+function addItem() { 
+    // Crear un nuevo elemento de la lista 
+    var li = document.createElement("li"); 
+    li.className = "list-group-item d-flex justify-content-between align-items-center ms-auto";
+    var innput = document.getElementById("btnAdd"); 
+    li.textContent = innput.value;
+    innput.value = "";    
 
+    // Crear el contenedor de la tarea 
+    var taskContainer = document.createElement("div"); 
+    taskContainer.className = "d-flex align-items-center";
+
+    // Crear el checkbox 
+    var checkbox = document.createElement("input"); 
+    checkbox.type = "checkbox"; 
+    checkbox.className = "form-check-input"; 
+    taskContainer.appendChild(checkbox);
+
+    // Crear el contenedor de los íconos 
+    var iconContainer = document.createElement("div"); 
+    iconContainer.className = "ms-auto relative";
+
+    // Crear el ícono de editar 
+    var edit = document.createElement("i"); 
+    edit.className = "fas fa-pencil-alt m-1"; 
+    edit.onclick = function() { alert("Edit"); }; 
+    iconContainer.appendChild(edit);
+
+    // Crear el ícono de eliminar 
+    var del = document.createElement("i"); 
+    del.className = "fas fa-trash-alt"; 
+    del.onclick = function() { li.remove(); }; 
+    iconContainer.appendChild(del); 
+
+    // Añadir el checkbox y el contenido de la tarea al contenedor de la tarea 
+    taskContainer.appendChild(li.firstChild); 
+    taskContainer.appendChild(iconContainer); 
+
+    // Añadir el contenedor de la tarea al elemento de la lista 
+    li.appendChild(taskContainer); 
+
+    // Añadir el elemento de la lista al ul 
     document.getElementById("list_tasks").appendChild(li);
-    li.className = "justify-content-between align-items-center";
-
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.className = "form-check-input";
-    checkbox.value = "";
-
-    var label = document.createElement("label");
-    label.className = "form-check-label";
-    label.appendChild(checkbox);
     
-    var del = document.createElement("i");
-    del.className = "fas fa-trash-alt";
-    del.onclick = function(){
-        li.remove();
-    }
-    label.appendChild(del);
-
-    var edit = document.createElement("i");
-    edit.className = "fas fa-pencil-alt";
-    edit.onclick = function(){
-        alert("Edit");
-    }
-    label.appendChild(edit);
-
-    document.getElementById("list_tasks").appendChild(label);
+    iconContainer.style.position = "absolute";
+    iconContainer.style.right = 0;
 }
-
-
-/*<a href="#!" class="text-info" data-mdb-tooltip-init title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a>
-<a href="#!" class="text-danger" data-mdb-tooltip-init title="Delete todo"><i class="fas fa-trash-alt"></i></a>
-*/
-/*
- funcion que crea un checkbox al elemento creado
- */
-/*function createCheckbox(){
-    var checkbox = document.createElement("input");
-
-    checkbox.type = "checkbox";
-    checkbox.className = "form-check-input";
-    checkbox.value = "";
-
-    var label = document.createElement("label");
-    label.className = "form-check-label";
-    label.appendChild(checkbox);
-
-    document.getElementById("list_tasks").appendChild(label);
-}*/
 
 function  main(){
     addItem();
